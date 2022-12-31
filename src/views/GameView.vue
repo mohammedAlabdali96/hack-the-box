00345a8e7839c9e5f4375399a1c5fb234f2ef5ee
Mohammed Alabdali <ddaf051@gmail.com>
2022-12-31 18:24:34 +0200

    create game view component

---
+++ src/views/GameView.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>this is game view</h1>
+</template>
+<script>
+export default ({
+  name: "GameView",
+  data() {
+    return {
+    };
+  },
+  mounted() {
+    console.log(this.$route.params.id)
+  },
+});
+</script>

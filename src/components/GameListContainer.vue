46d6202533238013f647ead68c48f0393b85b9ee
Mohammed Alabdali <ddaf051@gmail.com>
2022-12-31 18:23:37 +0200

    create container wrapper for game items

---
+++ src/components/GameListContainer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="game-list-container">
+    <game-item></game-item>
+  </div>
+</template>
+<script>
+import GameItem from "./GameItem.vue";
+export default {
+  name: "GameListContainer",
+  components: {
+    GameItem,
+  },
+};
+</script>
+<style lang="scss">
+.game-list-container {
+  margin-top: 16px;
+}
+</style>

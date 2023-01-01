5845cdffbaebacdc722866d8e44389263f424249
Mohammed Alabdali <ddaf051@gmail.com>
2023-01-01 19:04:55 +0200

    get the carousel images& video

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -8,6 +8,21 @@ export default ({
     return {
     };
   },
+    getCarouselItems() {
+      if (Object.keys(this.game).length !== 0) {
+        const items = this.game.screenshots.slice();
+        items.unshift({
+          id: this.game.movies.id,
+          path_thumbnail: this.game.movies[0].thumbnail,
+          path_full: this.game.movies[0].mp4["480"],
+        });
+        items.map((x, index) =>
+          index === 0 ? (x["isVideo"] = true) : (x["isVideo"] = false)
+        );
+        return items;
+      } else return {};
+    },
+  },
   mounted() {
     console.log(this.$route.params.id)
   },
